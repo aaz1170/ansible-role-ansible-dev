da5ecf5a4ac76e165e08067aa2cab2bdf8519829
Andreas Martin <a.martin@admin-auf-zeit.de>
2020-03-11 14:21:42 +0100

    Test and create venv with ansible packages

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -14,9 +14,11 @@ def test_hosts_file(host):
     assert f.user == 'root'
     assert f.group == 'root'
 
+
 def test_dependency_docker_is_appliyed(host):
     assert host.package("docker-ce").is_installed
     assert host.service("docker").is_running
+
 
 def test_packages_are_installed(host):
     packageList = [
@@ -28,7 +30,22 @@ def test_packages_are_installed(host):
     for item in packageList:
         assert host.package(item).is_installed
 
+
 def test_ansible_is_pinned_for_apt(host):
     cmd = host.run('sudo apt install ansible')
     assert "E: Package 'ansible' has no installation candidate" in cmd.stderr
     assert cmd.rc == 100
+
+
+def test_venv_is_created(host):
+    venv = host.file('/home/vagrant/.venv-ansible/bin/activate')
+    assert venv.exists
+    assert venv.user == 'vagrant'
+    assert venv.group == 'vagrant'
+
+
+def test_ansible_is_installed_in_venv(host):
+    packages = host.pip_package.get_packages(pip_path='/home/vagrant/.venv-ansible/bin/pip')
+    assert packages["ansible"]
+    assert packages["molecule"]
+    
